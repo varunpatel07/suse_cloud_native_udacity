83377df4b6a67e6633dc538a6801c7b063d33518
varun <varunpatel5555@gmail.com>
2021-06-28 21:35:29 +0530

    Excercise 2

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,5 +6,24 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/metric")
+def metric():
+    response=app.response_class(
+        response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
+        )
+    return response
+
 if __name__ == "__main__":
+    app.debug=True  
     app.run(host='0.0.0.0')
