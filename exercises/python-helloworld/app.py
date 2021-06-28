5e034d77b9dbc5692e585c5be3301135fe631f93
varun <varunpatel5555@gmail.com>
2021-06-28 23:07:11 +0530

    lesson-2 excercise part 2

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,13 +1,19 @@
 import json
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    #app.logger.info("MAIN CALLED")
+    logging.debug("Main called")
+
     return "Hello World!"
 
 @app.route("/status")
 def status():
+    #app.logger.info("STATUS CALLED")
+    logging.info("status called")
     response = app.response_class(
         response=json.dumps({"result":"OK - healthy"}),
 
@@ -16,6 +22,8 @@ def status():
 
 @app.route("/metric")
 def metric():
+    #app.logger.info("METRIC CALLED")
+    logging.info("metrics called")
     response=app.response_class(
         response=json.dumps({"data":{"UserCount":200,"UserCountActive":23}}),
         status=200,
@@ -24,5 +32,7 @@ def metric():
     return response
 
 if __name__ == "__main__":
+    handler=[logging.FileHandler(r'D:\udacity\exercises\python-helloworld\app.log'),logging.StreamHandler()]
+    logging.basicConfig(level=logging.INFO,handlers=handler)
     app.debug=True  
     app.run(host='0.0.0.0')
